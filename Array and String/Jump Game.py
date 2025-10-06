b8c8480b74fdaddec2bd175d894ed59f6a17964e
prakashRajs2709 <rajsp6037@gmail.com>
2025-10-06 18:01:17 +0530

    Add solutions for "Best Time to Buy and Sell Stock" and "Jump Game"

---
+++ Array and String/Jump Game.py
@@ -0,0 +1,7 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        fp=len(nums)-1
+        for i in range(len(nums)-2,-1,-1):
+            if i+nums[i]>=fp:
+                fp=i
+        return fp==0
