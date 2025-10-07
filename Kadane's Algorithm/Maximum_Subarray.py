c91831d0d985597cb5ccd2c057942f8d95f5ec07
prakashRajs2709 <rajsp6037@gmail.com>
2025-10-07 18:48:37 +0530

    Add solution for "Maximum Subarray" problem using Kadane's Algorithm

---
+++ Kadane's Algorithm/Maximum_Subarray.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxSubArray(self, nums):
+        max_sum = float('-inf')
+        cur_sum = 0
+        lst = []
+        temp = []
+        for i in nums:
+            cur_sum += i
+            temp.append(i)
+            if cur_sum>max_sum:
+                max_sum = max(max_sum,cur_sum)
+                lst = temp[:]
+            if cur_sum<0:
+                cur_sum = 0
+                temp = []
+        return max_sum,lst
+    
+sol = Solution()
+print(sol.maxSubArray(nums=[-4,3,5,2,-1]))
