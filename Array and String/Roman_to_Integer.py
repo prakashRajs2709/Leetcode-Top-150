1d2be92c49dff41139fe2fb5ed2181e329a96cf0
prakashRajs2709 <rajsp6037@gmail.com>
2025-10-09 17:24:29 +0530

    Refactor roman_to_integer function to be a method of the Solution class

--- Array and String/Roman_to_Integer.py
+++ Array and String/Roman_to_Integer.py
@@ -1,16 +1,15 @@
-def roman_to_integer(num)-> int:
-    result = 0
-    intmap = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-    for i in range(len(num)):
-        if i>0 and intmap[num[i]]>intmap[num[i-1]]:
-            result = result + intmap[num[i]] - 2 * intmap[num[i-1]]
-        else:
-            result = result + intmap[num[i]]
-    return result
-
-
-
-
+from typing import List
+class Solution:
+    def roman_to_integer(self, num: str)-> int:
+        result = 0
+        intmap = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        for i in range(len(num)):
+            if i>0 and intmap[num[i]]>intmap[num[i-1]]:
+                result = result + intmap[num[i]] - 2 * intmap[num[i-1]]
+            else:
+                result = result + intmap[num[i]]
+        return result
 
 num = 'XIV'
-print(roman_to_integer(num))
+sol = Solution()
+print(sol.roman_to_integer(num))
